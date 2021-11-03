dd4f5b1533ab49d2f1d91b89dd17892eab942479
Juliana Ifionu <julieifionu@gmail.com>
2021-11-03 14:07:43 +0100

    Create MyList class and add methods.

---
+++ MyList.rb
@@ -0,0 +1,12 @@
+require_relative "MyEnumerable"
+
+class MyList
+    include MyEnumerable
+    def initialize (*list)
+        @list = list
+        puts @list
+    end
+    def each
+        @list.each { |n| yield n }
+    end
+end
